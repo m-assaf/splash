9ceb4f06007952ac4d78def768c58975b2dd8f94
massaf-it <mahmoud.assaf@fluance.ch>
2014-08-27 16:16:08 +0200

    tret

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,6 @@
-  // Wait for device API libraries to load
-    //
+  
     document.addEventListener("deviceready", onDeviceReady, false);
 
-    // device APIs are available
-    //
     function onDeviceReady() {
         navigator.splashscreen.hide();
         setTimeout(function() {
