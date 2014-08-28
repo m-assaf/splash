af77cb51894628a61119913de86c39d6a37e6ea9
massaf-it <mahmoud.assaf@fluance.ch>
2014-08-28 10:10:07 +0200

    dsad

--- www/js/index.js
+++ www/js/index.js
@@ -38,31 +38,14 @@ var app = {
         setTimeout(function() {
             navigator.splashscreen.hide();
         }, 6000);
-
-        /*
-        navigator.splashscreen.show();
-
-        setTimeout(function() {
-
-                   navigator.splashscreen.hide();
-
-                   }, 4000); 
-                    */
-
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        
-        
-
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
-
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-
         console.log('Received Event: ' + id);
-
     }
 };
