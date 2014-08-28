e34024fe1ae24ee4c9974a72b85e2e95e8d37cde
massaf-it <mahmoud.assaf@fluance.ch>
2014-08-28 11:17:55 +0200

    sds

--- www/js/index.js
+++ www/js/index.js
@@ -47,5 +47,6 @@ var app = {
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
         console.log('Received Event: ' + id);
+        alert('id' + id);
     }
 };
